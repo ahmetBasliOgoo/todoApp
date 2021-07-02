842072eaa4d081b0b9a8085438894fa8b750e7eb
Ahmet Başlı <ahmetbasli@ogoodigital.com>
2021-07-02 11:56:45 +0300

    for the sake of readable code :)

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ $('.container__input').on('keyup', function(e){
         const trash = $('<i class="container__task__icon  fas fa-trash-alt"></i>').on('click', function(e){
             task.remove()
         })
-        const check =   $('<i class="container__task__icon fas fa-check"></i>').on('click', function(e){
+        const check = $('<i class="container__task__icon fas fa-check"></i>').on('click', function(e){
             task.removeClass('container__task--not-completed')
             task.addClass('container__task--completed')
             $('#completed').append(task)
